b239b46e5114f3d1978ec0c1d417ed181f7b6952
Sthefany Alaminos <alaminossthefany2@gmail.com>
2026-03-17 19:25:15 -0300

    Create translation.py

--- Dados com Python - Alura/app.py
+++ Dados com Python - Alura/app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 import plotly.express as px
+from translation import translation 
 
 # --- Configuração da Página ---
 # Define o título da página, o ícone e o layout para ocupar a largura inteira.
---
+++ Dados com Python - Alura/translation.py
@@ -0,0 +1,44 @@
+translations = {
+
+    "pt": {
+        "language_selector": "🌐 Idioma / Language",
+        "filters": "🔍 Filtros",
+        "year": "Ano",
+        "seniority": "Senioridade",
+        "contract": "Tipo de Contrato",
+        "company_size": "Tamanho da Empresa",
+
+        "title": "🎲 Dashboard de Análise de Salários na Área de Dados",
+        "description": "Explore os dados salariais na área de dados nos últimos anos. Utilize os filtros à esquerda para refinar sua análise.",
+
+        "metrics": "Métricas gerais (Salário anual em USD)",
+        "avg_salary": "Salário médio",
+        "max_salary": "Salário máximo",
+        "total_records": "Total de registros",
+        "most_common_role": "Cargo mais frequente",
+
+        "charts": "Gráficos",
+        "detailed_data": "Dados Detalhados"
+    },
+
+    "en": {
+        "language_selector": "🌐 Language / Idioma",
+        "filters": "🔍 Filters",
+        "year": "Year",
+        "seniority": "Seniority",
+        "contract": "Contract Type",
+        "company_size": "Company Size",
+
+        "title": "🎲 Data Salaries Dashboard",
+        "description": "Explore salary data in the data field over recent years. Use the filters on the left to refine your analysis.",
+
+        "metrics": "General Metrics (Annual salary in USD)",
+        "avg_salary": "Average salary",
+        "max_salary": "Maximum salary",
+        "total_records": "Total records",
+        "most_common_role": "Most frequent role",
+
+        "charts": "Charts",
+        "detailed_data": "Detailed Data"
+    }
+}
